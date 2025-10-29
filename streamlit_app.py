8b0e1384cb18be3c3b038df6b2a74f0b6b2007cf
AMeiwah <ANDY.MEI@student.mec.cuny.edu>
2025-10-29 11:15:31 -0400

    copy and paste

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,18 @@
 import streamlit as st
-import pandas as pd
+import pandas as pd 
 from datetime import datetime
 
 # Page Config
 st.set_page_config(
-  page_title = 'Andy | Portfolio',
-  page_icon="😊",
+  page_title ='Avinash Jairam | Portfolio',
+  page_icon='🎯',
   layout = 'wide'
 )
+
+# Custom CSS (optional - for styling)
+st.markdown('''
+                <style>
+                    .main-header {font-size: 42px; font-weight: bold; text-align:center;}
+                    .sub-header {font_size: 24px; text-align:center; color: #666;}
+                </style>
+            ''', unsafe_allow_html = True)
