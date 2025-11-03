c2e79682f5b44fa1f3c88b38d8507502ffc1a690
AMeiwah <ANDY.MEI@student.mec.cuny.edu>
2025-11-03 10:40:13 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ st.markdown('''
 # Sidebar
 st.sidebar.title('🌎 Navigation')
 page = st.sidebar.radio('Go to',
-                        {'🦾 Home','🐼 About', '📚 Projects', '🚗 Skills', '✈ Resume', '📞 Contact' })
+                        ['🦾 Home','🐼 About', '📚 Projects', '🚗 Skills', '✈ Resume', '📞 Contact' ])
 
 # Home page
 if page == '🦾 Home':
