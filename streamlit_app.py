388531814af5dffa0031908a0248b6afd338e9f7
AMeiwah <ANDY.MEI@student.mec.cuny.edu>
2025-11-29 23:23:26 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -95,7 +95,8 @@ elif page == '📚 Projects':
         with col1:
             st.image('download.jpeg')
         with col2:   
-            st.subheader("[Final ELA Essay 150](Final ELA Essay 150)")
+            st.subheader("Final ELA Essay 150")
+            st.write("[Final ELA Essay 150](Final ELA Essay 150)")
             st.write('A Review on the Assassin\'s Creed 2')
             st.caption('**Technologies:** Google Docs')
    
